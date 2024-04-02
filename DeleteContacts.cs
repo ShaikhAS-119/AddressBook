a9b9aef0776bf33839377a691b7df9037939e6ff
Shaikh Abid <alishaikh4199@gmail.com>
2024-04-02 22:16:35 +0530

    [abid]added:This file delete contacts

---
+++ DeleteContacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    internal class DeleteContacts
+    {
+        public static void delete()
+        {
+            List<Contacts> list = AddressbookMain.list;
+            Contacts contacts = new Contacts();
+
+            Console.WriteLine("************************************");
+            Console.WriteLine("DELETE THE CONTACTS:-");
+            Console.WriteLine();
+            Console.WriteLine("Enter the full name : ");
+            string name = Console.ReadLine().ToUpper();
+
+            try
+            {
+
+                foreach (var items in list)
+                {
+                    string value = items.Name.ToString().ToUpper();
+                    if (value == name)
+                    {
+                        list.Remove(items);
+                        Console.WriteLine("----------After removing---------");
+                        PrintAddBook.print();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} not found!!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+ 
+            }
+        }
+    }
+}
