262e482d3d1350286a70ce83bca5b2a9f9099499
Shaikh Abid <alishaikh4199@gmail.com>
2024-04-02 21:42:14 +0530

    [abid]added:Contact details

---
+++ Contacts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    internal class Contacts
+    {
+
+        public string Add { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public string Zipcode { get; set; }
+
+        public string PhoneNum { get; set; }
+
+        public string Email { get; set; }
+        //public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+
+        public Contacts()
+        {
+
+        }
+        public Contacts(string name, string add, string city, string state, string code, string phone, string mail)
+        {
+            this.Name = name;
+            this.City = city;
+            this.State = state;
+            this.PhoneNum = phone;
+            this.Email = mail;
+            this.Zipcode = code;
+            this.Add = add;
+
+        }
+    }
+}
