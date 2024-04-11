ede065c4a7279863ebe7585bef641f2edc70cf26
Shaikh Abid <alishaikh4199@gmail.com>
2024-04-11 17:31:52 +0530

    [abid]added: sorting persons by name

---
+++ SortByName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace AddressBook
+{
+    internal class SortByName
+    {
+        static List<Contacts> list = AddressbookMain.list;
+        public static void SortName()
+        {
+            Console.WriteLine("---------After sorting By Name---------");
+            Console.WriteLine();
+            var sorted = list.OrderBy(x => x.Name).ToList();
+            foreach (var contact in sorted)
+            {                
+                Console.WriteLine($"Nmae: {contact.Name.ToUpper()}\nAddress: {contact.Add}\nCity: {contact.City}\nState: {contact.State}\nZipCode: {contact.Zipcode}\nPhone: {contact.PhoneNum}\nEmail: {contact.Email}\n");
+            }
+           
+        }
+    }
+}
